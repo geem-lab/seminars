90d75495255d3fd4080ca1fb01cdfdee08ea48b2
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2022-03-08 16:22:50 -0300

    Improve seminar output

--- scripts/update-seminars.py
+++ scripts/update-seminars.py
@@ -1,7 +1,63 @@
 import os
+from dataclasses import dataclass
+from datetime import datetime
 
 import requests
 from dateutil import parser as dateutil_parser
+from markdown import markdown
+
+
+@dataclass
+class Seminar:
+    title: str
+    speaker: str
+    description: str
+    date: datetime
+
+    def _date_to_markdown(self):
+        return f"""
+        <p>
+            <strong>
+                {self.date.strftime('%d/%m/%Y')}
+            </strong>
+        </p>
+        """
+
+    def _title_to_markdown(self):
+        return f"""
+        <h2>
+            {self.title}
+        </h2>
+        """
+
+    def _speaker_to_markdown(self):
+        return f"""
+        <p>
+            <strong>
+                <a href="https://github.com/{self.speaker}">
+                    {self.speaker}
+                </a>
+            </strong>
+        </p>
+        """
+
+    def _description_to_markdown(self):
+        return f"""
+        {markdown(self.description)}
+        """
+
+    def to_markdown(self):
+        return f"""
+        <details>
+            <summary>
+                {self._date_to_markdown()}
+                {self._title_to_markdown()}
+            </summary>
+            {self._speaker_to_markdown()}
+            {self._description_to_markdown()}
+        </details>
+        """
+
 
 if __name__ == "__main__":
     token = os.environ["GITHUB_TOKEN"]
@@ -18,7 +74,6 @@ if __name__ == "__main__":
 
     gh_session = requests.Session()
     gh_session.auth = (owner, token)
-
     issues = gh_session.get(query_url, headers=headers, params=params).json()
 
     seminars = []
@@ -33,11 +88,9 @@ if __name__ == "__main__":
             date = dateutil_parser.parse(date)
             speaker = issue["assignees"][0]["login"]
 
-            seminar = {"title": title, "body": body, "date": date, "speaker": speaker}
+            seminar = Seminar(title=title, speaker=speaker, description=body, date=date)
             seminars.append(seminar)
 
-    seminars = sorted(seminars, key=lambda s: s["date"])
+    seminars = sorted(seminars, key=lambda s: s.date)
     for seminar in seminars:
-        print(f"- **{seminar['title']}** (*{seminar['date'].strftime('%Y-%m-%d')}*)")
-        print(f"  Speaker: @{seminar['speaker']}.")
-        print(f"  {seminar['body']}")
+        print(seminar.to_markdown())
