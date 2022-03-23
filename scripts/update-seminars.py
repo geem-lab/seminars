21f7c9585f120402414ce23749c14bddb4ba2228
Leticia-maria <leticiapequeno30@gmail.com>
2022-03-23 12:04:57 -0300

    Remove IPython

--- scripts/update-seminars.py
+++ scripts/update-seminars.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import os
 from dataclasses import dataclass
 from datetime import datetime
-from IPython.display import display,HTML
 import calendar
 import dateparser
 import requests
